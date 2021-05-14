7c59998e072ff2be24fc2294f8fba47b3c67da2e
Rafaela Polizel <polizelc.rafaela@gmail.com>
2021-05-13 21:36:55 -0300

    Removing unused card repository

--- src/main/java/br/com/zupacademy/rafaela/proposta/Card/CardRepository.java
+++
@@ -1,8 +0,0 @@
-package br.com.zupacademy.rafaela.proposta.Card;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CardRepository extends JpaRepository<Card, Long> {
-}
--- src/main/java/br/com/zupacademy/rafaela/proposta/services/CardResourceService/CardResourceScheduler.java
+++ src/main/java/br/com/zupacademy/rafaela/proposta/services/CardResourceService/CardResourceScheduler.java
@@ -1,7 +1,6 @@
 package br.com.zupacademy.rafaela.proposta.services.CardResourceService;
 
 import br.com.zupacademy.rafaela.proposta.Card.Card;
-import br.com.zupacademy.rafaela.proposta.Card.CardRepository;
 import br.com.zupacademy.rafaela.proposta.Proposal.Proposal;
 import br.com.zupacademy.rafaela.proposta.Proposal.ProposalRepository;
 import br.com.zupacademy.rafaela.proposta.utils.enums.ProposalStatusEnum;
@@ -9,8 +8,6 @@ import br.com.zupacademy.rafaela.proposta.utils.transactions.TransactionExecutor
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
-import java.util.List;
-import java.util.Optional;
 import java.util.Set;
 
 @Component
@@ -18,13 +15,11 @@ public class CardResourceScheduler {
     private final CardResourceClient cardResourceClient;
     private final TransactionExecutor transaction;
     private final ProposalRepository proposalRepository;
-    private final CardRepository cardRepository;
 
-    public CardResourceScheduler(CardResourceClient cardResourceClient, TransactionExecutor transaction, ProposalRepository proposalRepository, CardRepository cardRepository){
+    public CardResourceScheduler(CardResourceClient cardResourceClient, TransactionExecutor transaction, ProposalRepository proposalRepository){
         this.cardResourceClient = cardResourceClient;
         this.transaction = transaction;
         this.proposalRepository = proposalRepository;
-        this.cardRepository = cardRepository;
     }
 
     @Scheduled(fixedDelayString = "${scheduler.card.resource}")
